6772dcaa6fd9286869b18d79295a09b5bd2e2254
faouellet <faouellet@gmail.com>
2015-08-02 09:03:09 -0400

    Adding function comment genenration

---
+++ DoxygenTemplateGenerator/main.cpp
@@ -0,0 +1,91 @@
+#include <string>
+#include <sstream>
+
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::driver;
+using namespace clang::tooling;
+
+static llvm::cl::OptionCategory FuncDeclMatcherCategory("Func Decl Matcher");
+
+class FuncDeclHandler : public MatchFinder::MatchCallback {
+public:
+  FuncDeclHandler(Rewriter &Rewrite) : TheRewriter(Rewrite) {}
+
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    // We have a match
+    if (const FunctionDecl *FDecl =
+            Result.Nodes.getNodeAs<clang::FunctionDecl>("funcDecl")) {
+      // Only considering function definitions
+      if (FDecl->hasBody()) {
+        // Formatting the doxygen comment
+        std::stringstream SStream;
+        SStream << "/**\n";
+        SStream << "* \\fn " << FDecl->getNameInfo().getAsString() << "\n";
+        SStream << "* \\brief\tFUNCTION DESCRIPTION\n";
+        for (auto &Param : FDecl->parameters())
+          SStream << "* \\param " << Param->getName().str() << "\tPARAM DESC\n";
+        SStream << "* \\return\tRETURN DESCRIPTION\n";
+        SStream << "*/\n";
+
+        SourceLocation SrcLoc = FDecl->getSourceRange().getBegin();
+        TheRewriter.InsertText(SrcLoc, SStream.str(), /*InsertAfter=*/true,
+                               /*IndentNewLines*/ true);
+      }
+    }
+  }
+
+private:
+  Rewriter &TheRewriter;
+};
+
+class MyASTConsumer : public ASTConsumer {
+public:
+  MyASTConsumer(Rewriter &R) : HandlerForFuncDecl(R) {
+    Matcher.addMatcher(typedefDecl().bind("funcDecl"), &HandlerForFuncDecl);
+  }
+
+  void HandleTranslationUnit(ASTContext &Context) override {
+    Matcher.matchAST(Context);
+  }
+
+private:
+  FuncDeclHandler HandlerForFuncDecl;
+  MatchFinder Matcher;
+};
+
+class MyFrontendAction : public ASTFrontendAction {
+public:
+  MyFrontendAction() {}
+  void EndSourceFileAction() override {
+    TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID())
+        .write(llvm::outs());
+  }
+
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                 StringRef) override {
+    TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+    return llvm::make_unique<MyASTConsumer>(TheRewriter);
+  }
+
+private:
+  Rewriter TheRewriter;
+};
+
+int main(int argc, const char **argv) {
+  CommonOptionsParser op(argc, argv, FuncDeclMatcherCategory);
+  ClangTool tool(op.getCompilations(), op.getSourcePathList());
+
+  return tool.run(newFrontendActionFactory<MyFrontendAction>().get());
+}
